67af7dfd3a6f851450ba466b82a75f4bbcad07ee
AlperenSarinay <39926184+AlperenSarinay@users.noreply.github.com>
2020-02-14 16:12:55 -0600

    project :: added app

---
+++ CenikLab/ribobase/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+# Create your views here.
+
+def register(request):
+
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username = username)
+        newUser = User(email = email)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request,newUser)
+        
+
+        return redirect("index")
+    context =  { 
+        "form" : form
+    }
+    return render(request,"register.html",context)
+
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(email = email,password = password)
+        if user is   None:
+            messages.info(request,"Error !!!!!")
+            return render(request,"login.html",context) 
+
+        login(request,user)
+        return redirect("index")
+       
+           
+    return render(request,"login.html",context)
+
+def logoutUser(request):
+    return render(request,"logout.html")
