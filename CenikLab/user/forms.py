952d7cb528051f966276b9950d2fab0c43682eb4
AlperenSarinay <39926184+AlperenSarinay@users.noreply.github.com>
2020-05-28 17:30:38 +0300

    login && register :: bug fixed

--- CenikLab/user/forms.py
+++ CenikLab/user/forms.py
@@ -1,34 +1,47 @@
 from django import forms
 
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate
+
 #form structures required for users' information
 class LoginForm(forms.Form):
-    email = forms.CharField(label = "E-mail")
+    username = forms.CharField(label = "Username")
     password = forms.CharField(label = "Password", widget = forms.PasswordInput)
 
-#form structures required for users' information
-class RegisterForm(forms.Form):
-
-    username = forms.CharField(max_length = 50, label="Username")
-    email = forms.CharField(max_length = 50, label="E-mail")
-    password = forms.CharField(max_length = 20, label="Password",widget = forms.PasswordInput)
-    confirm = forms.CharField(max_length = 20, label = "confirm Password", widget = forms.PasswordInput)
 
     def clean(self):
         username = self.cleaned_data.get("username")
-        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        if username and password:
+            user = authenticate(username = username,password = password)
+            if not user:
+                raise forms.ValidationError("yanlis")
+        
+        return super(LoginForm,self).clean()
+
+#form structures required for users' information
+class RegisterForm(forms.ModelForm):
+
+    username = forms.CharField(max_length = 50, label="Username")
+    password = forms.CharField(max_length = 20, label="Password",widget = forms.PasswordInput)
+    confirm = forms.CharField(max_length = 20, label = "confirm Password", widget = forms.PasswordInput)
+
+    class Meta:
+       model = User
+       fields = [
+           'username',
+           'password',
+           'confirm',
+       ]
+
+    def clean_confirm(self):
         password = self.cleaned_data.get("password")
         confirm = self.cleaned_data.get("confirm")
-
         if password and confirm and password != confirm:
             raise forms.ValidationError("password does not match")
+        return confirm
 
-        values = {
-            "username":username,
-            "email":email,
-            "password":password,
-
-        }
-        return values
 
 
 
--- CenikLab/user/views.py
+++ CenikLab/user/views.py
@@ -4,6 +4,7 @@ from .forms import RegisterForm,LoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate
+from .forms import LoginForm,RegisterForm
 
 ##Login Page and Register Page backed work is done on this views page.##
 # Create your views here.
@@ -13,23 +14,15 @@ def register(request):
 
     form = RegisterForm(request.POST or None)
     if form.is_valid():
-        username = form.cleaned_data.get("username") #Checking if the form is empty
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username = user.username,password=password)
+        login(request,new_user)
+        return redirect('index')
 
-        newUser = User(username = username)
-        newUser = User(email = email)
-        newUser.set_password(password)
-
-        newUser.save()  #created newUser
-        login(request,newUser)
-        
-
-        return redirect("index")
-    context =  { 
-        "form" : form
-    }
-    return render(request,"register.html",context)
+    return render(request,"register.html",{'form':form})
 
 #code that allows the registered user to enter the system
 def loginUser(request):
@@ -38,18 +31,12 @@ def loginUser(request):
         "form":form
     }
     if form.is_valid():  #Checking if the form is empty
-        email = form.cleaned_data.get("email")
+        username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
 
-        user = authenticate(email = email,password = password)
-        if user is   None:  # If user is empty it won't do anything
-            messages.info(request,"Error !!!!!")
-            return render(request,"login.html",context) 
-
+        user = authenticate(username = username,password = password)
         login(request,user)
-        return redirect("index")
-       
-           
+        return redirect("index")     
     return render(request,"login.html",context)
 
 def logoutUser(request):
